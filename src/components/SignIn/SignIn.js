6ef4c2b8db1986a37cd7f3dc442fb56ca8637af8
Le Thanh <home@Les-iMac.local>
2020-09-24 21:44:13 +0700

    intergated firebase into app

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -44,11 +44,12 @@ class SignIn extends Component {
 						handeChange={this.handleChange}
 						label='password'
 					/>
-
-					<CustomButton type='submit'>Sign In</CustomButton>
-					<CustomButton onClick={signInWithGoogle}>
-						Sign In with Google
-					</CustomButton>
+					<div className='buttons'>
+						<CustomButton type='submit'>Sign In</CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+							Sign In with Google
+						</CustomButton>
+					</div>
 				</form>
 			</div>
 		);
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => {
+const Header = ({ currentUser }) => {
 	return (
 		<div className='header'>
 			<Link to='/' className='logo-container'>
@@ -16,6 +17,15 @@ const Header = () => {
 				<Link to='/contact' className='option'>
 					CONTACT
 				</Link>
+				{currentUser ? (
+					<div className='option' onClick={() => auth.signOut()}>
+						SIGN OUT
+					</div>
+				) : (
+					<Link className='option' to='/signin'>
+						SIGN IN
+					</Link>
+				)}
 			</div>
 		</div>
 	);
