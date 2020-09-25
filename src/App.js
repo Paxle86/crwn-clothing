f7ef19022eeda9b1939d78d62fee594ca56cc4a4
Le Thanh <home@Les-iMac.local>
2020-09-25 09:51:51 +0700

    check user auth

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header/Header';
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import SignInAndSignUp from './pages/sign-in and sign-up/SignIn-SignUp';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 	constructor() {
@@ -17,9 +17,10 @@ class App extends React.Component {
 	}
 
 	unsubcribeFromAuth = null;
+
 	componentDidMount() {
-		this.unsubcribeFromAuth = auth.onAuthStateChanged((user) => {
-			this.setState({ currentUser: user });
+		this.unsubcribeFromAuth = auth.onAuthStateChanged(async (user) => {
+			createUserProfileDocument(user);
 		});
 	}
 	componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,14 @@ const config = {
 	measurementId: 'G-J0YJ1S4FFG',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`user/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+	console.log(snapShot);
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
